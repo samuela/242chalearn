321333c399c1fc05165637d935704737e9c75e27
Samuel Ainsworth <skainswo@smp011.oscar.ccv.brown.edu>
2014-11-25 21:02:27 -0500

    push

--- classifySample.py
+++ classifySample.py
@@ -1,8 +1,9 @@
 from glob import glob
 import random
-from gatherData import gatherAllXY, gatherRandomXY
+from gatherData import gatherAllXY, gatherRandomXY, loadFile
 import numpy as np
 from scipy import stats
+from itertools import groupby
 
 from sklearn import svm
 from sklearn.metrics import confusion_matrix
@@ -22,16 +23,19 @@ def smooth(pred):
     frame_rate = 20
     smoothed = np.zeros(pred.shape)
     for i in range(smoothed.size):
-        neighbors = np.array([pred(x) for x in range(min(i-frame_rate/2, 0), max(i+frame_rate/2,0))])
+        min_index = max(i-frame_rate/2, 0)
+        max_index = min(i+frame_rate/2,smoothed.size)
+        neighbors = np.array([pred[x] for x in range(min_index, max_index)])
         smoothed[i] = stats.mode(neighbors)[0][0]
-    return smoothed
+    return [k for k, g in groupby(smoothed)]
 
 
-train_num_files = 20
-samples_per_file = 20
+train_num_files = 200
+samples_per_file = 200
 train_file_pattern = '/users/skainswo/data/skainswo/chalearn/train/Sample*_data.mat'
 all_train_files = glob(train_file_pattern)
 test_file = all_train_files[0]
+print "test file", test_file
 train_data_files = random.sample(all_train_files[1:], train_num_files)
 
 # validation_num_files = 200
@@ -57,9 +61,9 @@ print Xtrain.shape, len(Ytrain)
 
 def trainAndPredict(clf, fn):
     clf = clf.fit(Xtrain, Ytrain)
-    print "done training"
     pred = predict(clf,fn)
     return pred
 
 print 'training and predicting'
-print trainAndPredict(RandomForestClassifier(n_estimators=250, n_jobs=-1), test_file)
+print 'predicted', trainAndPredict(RandomForestClassifier(n_estimators=250, n_jobs=-1), test_file)
+print 'actual', [k for k, g in groupby(loadFile(test_file)['frame_labels']) if k > 0]
