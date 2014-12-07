6007db3d97b2c79a339da526f407cd19436d62e7
Samuel Ainsworth <samuel_ainsworth@brown.edu>
2014-12-06 21:33:54 -0500

    add my full HW3 code

--- SSP/solutionSam/lds/particle_filter_part_f.m
+++ SSP/solutionSam/lds/particle_filter_part_f.m
@@ -1,4 +1,4 @@
-function [X] = particle_filter(num_particles, model, data, x0, P0)
+function [X] = particle_filter_part_f(num_particles, model, data, x0, P0, corruption)
 %function [X] = particle_filter(num_particles, model, data, x0, P0)
 % Inputs:
 %    num_particles: number of particles to use
@@ -19,11 +19,11 @@ function [X] = particle_filter(num_particles, model, data, x0, P0)
   
   % P is D x num_particles
   P = mvnrnd(x0, P0, num_particles)';
-%   w = ones(num_particles, 1);
+  w = ones(num_particles, 1);
   for t=1:T
     % Re-weight particles
-%     w = w .* mvnpdf(data(t), (model.C * P)', model.R);
-    w = mvnpdf(data(t), (model.C * P)', model.R);
+    w = w .* (corruption * normpdf(data(t), 0, 40^2) ...
+        + (1 - corruption) * mvnpdf(data(t), (model.C * P)', model.R));
     w = w / sum(w);
     
     % Calculate posterior mean
@@ -32,7 +32,7 @@ function [X] = particle_filter(num_particles, model, data, x0, P0)
     % Resample
     ix = randsample(1:num_particles, num_particles, true, w);
     P = P(:,ix);
-%     w = ones(num_particles, 1) / num_particles;
+    w = ones(num_particles, 1) / num_particles;
     
     % Propogate via transition probability
     P = mvnrnd((model.A * P)', model.Q)';
---
+++ SSP/solutionSam/lds/q1.m
@@ -0,0 +1,147 @@
+clear variables;
+close all;
+
+seed = 0;
+rng(seed);
+figCount = 1;
+
+%% 1a %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+load('track', 'states', 'data', 'model', 'x0', 'P0');
+% Model is a data structure that contains fields: A,Q,R,C, which correspond
+% to the parameters defined in Question 1 of the handout.
+T = numel(data);
+
+[Xf, Pf] = kalman_smoother(model, data', x0, P0);
+close all;
+plot_truth(states, data, figCount); figCount = figCount + 1;
+hold on;
+E = shiftdim(2 * sqrt(Pf(1,1,:)), 1);
+errorbar(1:T, Xf(1,:), E, '.k');
+hold off;
+xlim([0, T+1]);
+title('Question 1(a)');
+legend('True State', 'Measurement', 'Kalman Filter');
+
+saveas(gcf, 'q1a.png');
+
+clear Xf Pf
+%% 1b %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% set initial conditions
+P0_1d = P0(1);
+x0_1d = x0(1);
+% Todo: MODIFY 'P0_1d' and 'x0_1d' to the corresponding 1-d initial conditions
+
+model_1d = model;
+% Todo: MODIFY 'model_1d' data structure to use the 1d case here
+model_1d.A = 1;
+model_1d.Q = 0.01/3;
+model_1d.C = 1;
+model_1d.R = 20;
+
+[Xf, Pf] = kalman_smoother(model_1d, data', x0_1d, P0_1d);
+plot_truth(states, data, figCount); figCount=figCount+1;
+hold on;
+E = shiftdim(2*sqrt( Pf(1,1,:) ), 1);
+errorbar( 1:T, Xf(1,:), E, '.k' );
+hold off;
+xlim([0, T+1]);
+legend('True State', 'Measurement', 'Kalman Filter');
+saveas(gcf, 'q1b1.png');
+
+model_1d = model;
+% Todo: MODIFY 'model_1d' data structure to use the 1d case here
+model_1d.A = 1;
+model_1d.Q = 10;
+model_1d.C = 1;
+model_1d.R = 20;
+
+[Xf, Pf] = kalman_smoother(model_1d, data', x0_1d, P0_1d);
+plot_truth(states, data, figCount); figCount = figCount + 1;
+hold on;
+E = shiftdim(2*sqrt( Pf(1,1,:) ), 1);
+errorbar( 1:T, Xf(1,:), E, '.k' );
+hold off;
+xlim([0, T+1]);
+legend('True State', 'Measurement', 'Kalman Filter');
+saveas(gcf, 'q1b2.png');
+
+clear x0_1d P0_1d model_1d
+
+%% 1d %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+numRuns = 3;
+nParticles = 100;
+
+plot_truth(states, data, figCount); figCount = figCount + 1;
+hold on;
+E = shiftdim(2*sqrt( Pf(1,1,:) ), 1);
+errorbar( 1:T, Xf(1,:), E, '.k' );
+% run particle filter
+for i=1:numRuns
+    X_pf = particle_filter(nParticles, model, data', x0, P0);
+    plot( 1:numel(data), X_pf(1,:), '.-c' );
+end
+
+xlim([0, T+1]);
+h=legend('True State', 'Measurement', 'Kalman Filter', 'Particle Filter', 'Location', 'northwest');
+% rect = [0.6, 0.25, .25, .25];
+% set(h, 'Position', rect)
+saveas(gcf, 'q1d.png');
+
+clear X_pf
+%% 1e %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+numRuns = 3;
+nParticles = 20;
+
+plot_truth(states, data, figCount); figCount = figCount + 1;
+hold on;
+E = shiftdim(2*sqrt( Pf(1,1,:) ), 1);
+errorbar( 1:T, Xf(1,:), E, '.k' );
+% run particle filter
+for i=1:numRuns
+    X_pf = particle_filter(nParticles, model, data', x0, P0);
+    plot( 1:numel(data), X_pf(1,:), '.-c' );
+end
+
+xlim([0, T+1]);
+h=legend('True State', 'Measurement', 'Kalman Filter', 'Particle Filter', 'Location', 'northwest');
+% rect = [0.6, 0.25, .25, .25];
+% set(h, 'Position', rect)
+saveas(gcf, 'q1e.png');
+
+clear X_pf
+
+%% 1f %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% corrupt data
+to_flip = rand(numel(data),1) < 0.1;
+noisy_vals = 40*randn(numel(data),1);
+noisy_data = (1-to_flip).*data + to_flip.*noisy_vals;
+
+% run Kalman smoother
+[Xf, Pf] = kalman_smoother(model, noisy_data', x0, P0);
+
+% run particle filter for the noisy observation model
+% Todo: modify your implementation of particle_filter.m to take into
+% account the outlier noise model
+
+% place your posterior-mean estimates in X_pf. The code below is just a
+% place holder for your code
+% X_pf = zeros(numel(x0),numel(data));
+nParticles = 100;
+
+% plot
+plot_truth(states, noisy_data, figCount); figCount = figCount + 1;
+hold on;
+E = shiftdim(2*sqrt( Pf(1,1,:) ), 1);
+errorbar( 1:T, Xf(1,:), E, '.k' );
+for i=1:3
+  X_pf = particle_filter_part_f(nParticles, model, data', x0, P0, 0.1);
+  plot( 1:T, X_pf(1,:), '.-c' );
+end
+hold off;
+xlim([0, T+1]);
+legend('True State', 'Measurement', 'Kalman Filter', 'Particle Filter', 'Location', 'northwest');
+saveas(gcf, ['q1f_' num2str(nParticles) '.png']);
